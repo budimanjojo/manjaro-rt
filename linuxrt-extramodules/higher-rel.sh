e6d74a96fcfc25084539d996663ee7edc518eff0
Budiman JoJo <budimanjojo@gmail.com>
2016-06-14 10:29:20 +0700

    delete old directories

--- linuxrt-extramodules/higher-rel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for pkg in $(ls */PKGBUILD | sed s'|/PKGBUILD||g') ; do
-   echo "Edit ${pkg}"
-   pushd ${pkg} &>/dev/null
-   _rel=$(cat PKGBUILD | grep pkgrel= | cut -d= -f2 | head -n1)
-   sed -i -e "s/pkgrel=${_rel}/pkgrel=$((${_rel}+1))/" PKGBUILD
-   popd &>/dev/null
-done
