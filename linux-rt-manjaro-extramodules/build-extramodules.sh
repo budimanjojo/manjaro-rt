fea2638e15a34144c0b2dc31e24363d082de47ed
Budiman JoJo <budimanjojo@gmail.com>
2016-07-16 10:42:27 +0700

    Modification: - bump to rt8 - bump extramodules - removed nvidia patch from nvidia module - bump to nvidia 367.35

--- linux-rt-manjaro-extramodules/build-extramodules.sh
+++ linux-rt-manjaro-extramodules/build-extramodules.sh
@@ -10,24 +10,24 @@ fi
 echo 'cleaning environment'
 rm -R ${pwd}/*/{src,pkg} -f
 echo 'building extramodules'
-cd ${pwd}/*acpi_call && makepkg --sign -sf --noconfirm
-cd ${pwd}/*bbswitch && makepkg --sign -sf --noconfirm
-cd ${pwd}/*broadcom-wl && makepkg --sign -sf --noconfirm
-cd ${pwd}/*catalyst && makepkg --sign -df --noconfirm
-cd ${pwd}/*ndiswrapper && makepkg --sign -sf --noconfirm
-cd ${pwd}/*nvidia && makepkg --sign -sf -d --noconfirm
-cd ${pwd}/*nvidia-304xx && makepkg --sign -d --noconfirm
-#cd ${pwd}/*nvidia-340xx && makepkg --sign -d --noconfirm
-cd ${pwd}/*nvidiabl && makepkg --sign -sf --noconfirm
-cd ${pwd}/*open-vm-tools-modules && makepkg --sign -sf --noconfirm
-#cd ${pwd}/*spl_zfs && makepkg --sign -sf --noconfirm
-cd ${pwd}/*r8168 && makepkg --sign -sf --noconfirm
-cd ${pwd}/*rt3562sta && makepkg --sign -sf --noconfirm
-cd ${pwd}/*tp_smapi && makepkg --sign -sf --noconfirm
-cd ${pwd}/*vhba-module && makepkg --sign -sf --noconfirm
-cd ${pwd}/*virtualbox-modules && makepkg --sign -sf --noconfirm
-echo 'create repo'
-mkdir -p ${pwd}/repo-`uname -m`
-mv ${pwd}/*/*`uname -m`.pkg* ${pwd}/repo-`uname -m`
-ls ${pwd}/repo-`uname -m`
-echo 'building extramodules done'
+cd ${pwd}/*acpi_call && makepkg -sf --noconfirm
+cd ${pwd}/*bbswitch && makepkg -sf --noconfirm
+cd ${pwd}/*broadcom-wl && makepkg -sf --noconfirm
+cd ${pwd}/*catalyst && makepkg -df --noconfirm
+cd ${pwd}/*ndiswrapper && makepkg -sf --noconfirm
+cd ${pwd}/*nvidia && makepkg -sf -d --noconfirm
+cd ${pwd}/*nvidia-304xx && makepkg -d --noconfirm
+#cd ${pwd}/*nvidia-340xx && makepkg -d --noconfirm
+cd ${pwd}/*nvidiabl && makepkg -sf --noconfirm
+cd ${pwd}/*open-vm-tools-modules && makepkg -sf --noconfirm
+#cd ${pwd}/*spl_zfs && makepkg -sf --noconfirm
+cd ${pwd}/*r8168 && makepkg -sf --noconfirm
+cd ${pwd}/*rt3562sta && makepkg -sf --noconfirm
+cd ${pwd}/*tp_smapi && makepkg -sf --noconfirm
+cd ${pwd}/*vhba-module && makepkg -sf --noconfirm
+cd ${pwd}/*virtualbox-modules && makepkg -sf --noconfirm
+#echo 'create repo'
+#mkdir -p ${pwd}/repo-`uname -m`
+#mv ${pwd}/*/*`uname -m`.pkg* ${pwd}/repo-`uname -m`
+#ls ${pwd}/repo-`uname -m`
+#echo 'building extramodules done'
