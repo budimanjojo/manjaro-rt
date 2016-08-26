ebf66ed129d58c3d2fe96db191a62b8f0ea77e35
Budiman JoJo <budimanjojo@gmail.com>
2016-08-26 18:43:35 +0700

    Update from upstream - bump extramodules - add elfutils as makedepends

--- linux-rt-manjaro-extramodules/build-extramodules.sh
+++ linux-rt-manjaro-extramodules/build-extramodules.sh
@@ -26,7 +26,7 @@ cd ${pwd}/*r8168 && makepkg -sf --noconfirm
 cd ${pwd}/*rt3562sta && makepkg -sf --noconfirm
 cd ${pwd}/*tp_smapi && makepkg -sf --noconfirm
 cd ${pwd}/*vhba-module && makepkg -sf --noconfirm
-#cd ${pwd}/*virtualbox-modules && makepkg -sf --noconfirm
+cd ${pwd}/*virtualbox-modules && makepkg -sf --noconfirm
 
 echo '  -> cleaning up ...'
 rm -R ${pwd}/*/{src,pkg} -f
